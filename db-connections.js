eb19c00a798e61706b37acd7c3c4c540d6872f3a
Anthonyyee123 <83298138+Anthonyyee123@users.noreply.github.com>
2022-08-18 17:04:31 +0800

    Add files via upload

---
+++ db-connections.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "restaurantreviewdb.c0p9vh0kgujb.us-east-1.rds.amazonaws.com",
+  port: "3306",
+  user: "admin",
+  password: "tG077032",
+  database: "comfieats",
+});
+
+connection.connect((err) => {
+  // test out connection and console.log error if there is one
+
+  if (err) throw err;
+
+  console.log("Connected To DB");
+});
+
+module.exports = connection;
